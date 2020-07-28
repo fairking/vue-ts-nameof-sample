c24c227023f0da47fbaee713366383a49748b5c5
dpujdak <denis.pujdak@gmail.com>
2020-07-28 08:54:11 +0100

    uncomment vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,21 +2,21 @@ const path = require("path");
 
 module.exports = {
     chainWebpack: config => {
-        //config.module
-        //    .rule('ts')
-        //    .exclude.add(/node_modules/)
-        //    .end()
-        //    .test(/\.ts$/)
-        //    .use('ts-loader')
-        //    .loader('ts-loader')
-        //    .options({
-        //        transpileOnly: true,
-        //        // Transformer functions do not work with happy pack mode due to process forking, see:
-        //        // https://github.com/TypeStrong/ts-loader#getcustomtransformers--program-program---before-transformerfactory-after-transformerfactory--
-        //        getCustomTransformers: path.resolve(__dirname, 'vue-ts-nameof.js'),
-        //        appendTsSuffixTo: ['\\.vue$'],
-        //        happyPackMode: true
-        //    })
-        //    .end();
+        config.module
+            .rule('ts')
+            .exclude.add(/node_modules/)
+            .end()
+            .test(/\.ts$/)
+            .use('ts-loader')
+            .loader('ts-loader')
+            .options({
+                transpileOnly: true,
+                // Transformer functions do not work with happy pack mode due to process forking, see:
+                // https://github.com/TypeStrong/ts-loader#getcustomtransformers--program-program---before-transformerfactory-after-transformerfactory--
+                getCustomTransformers: path.resolve(__dirname, 'vue-ts-nameof.js'),
+                appendTsSuffixTo: ['\\.vue$'],
+                happyPackMode: true
+            })
+            .end();
     }
 };
